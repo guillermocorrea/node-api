1c0f9081a3a66fcba1e2af614af2e53aa88e7fb7
Guillermo Correa <guille_freelance@hotmail.com>
2014-05-07 14:59:14 -0500

    initial commit

--- models/bear.js
+++ models/bear.js
@@ -1,3 +1,11 @@
 /**
  * Created by EXTIGLGCORREAG on 07/05/2014.
  */
+// app/models/bear.js
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+var BearSchema   = new Schema({
+    name: String
+});
+
+module.exports = mongoose.model('Bear', BearSchema);
